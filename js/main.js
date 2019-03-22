b347638d2c35fcaf17f52e18bfb900c7d9b284dc
Cronix <haos-sah@mail.ru>
2019-03-22 13:01:16 +0200

    comit

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ window.onload = function () {
 		document.getElementById('container').className = "verde";
 		document.getElementById('container').style.background = "red";
 	};
-	let paragraphs = document.getElementById("paragraph").children;
+	let paragraphs = document.getElementById('paragraph').children;
 	let i;
 	for (i = 0; i < paragraphs.length; i++) {
 		if (i % 2 == 0) {
@@ -28,22 +28,40 @@ window.onload = function () {
 		list = document.createElement('li');
 		document.body.appendChild(list);
 		list.innerHTML = 'Element NR ' + '{{' + i + '}}';
-		
+
 	};
 	console.log(list.innerHTML)
 	list.style.backgroundColor = "purple";
-	
-	var btn = document.createElement("button");
-    var text = document.createTextNode("CLICK ME");
 
-    btn.setAttribute("style","color:red;font-size:23px");
-
-    btn.appendChild(text);
-    document.body.appendChild(btn);
+	let btn = document.createElement('button');
+	let text = document.createTextNode('CLICK ME!');
+	btn.appendChild(text);
+	document.body.appendChild(btn);
+	btn.onclick = function (){
+		list.remove(5);
+	}
+	let x = 1;
+	let count = document.getElementById('cre')
+	count.onclick = function (){
+		count.innerHTML = 'Contor ' + x++ + ' ori';
+	}
+	let cerc = document.querySelectorAll(".cerc"); 
+	let a;
+	for (a = 0; a < cerc.length; a++) {
+	cerc[a].style.backgroundColor = "black";
+	}
 }
 
 setTimeout(function () {
+	let ul = document.createElement('ul')
+		document.body.appendChild(ul);
+		for(i = 0; 1< 10; i++){
+			let li = document.createElement('li')
+			document.body.appendChild(li);
+		}
+}, 5000); 
+setTimeout(function () {
 	document.getElementById('body').style.background = "gray";
-}, 3000);
+}, 5000);
 
 
